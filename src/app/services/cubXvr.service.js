8d4c1c20b11a71856656edb19b9dd09e5399ea46
Duck <juandavidp1127@gmail.com>
2024-10-17 15:57:50 -0500

    feat services on cubXVR

---
+++ src/app/services/cubXvr.service.js
@@ -0,0 +1,39 @@
+import http from "../../http-common";
+
+const route = "cub_x_vr";
+
+class CubXVrDataService {
+    getByCUB(id) {
+        return http.get(`/${route}/getByCUB/${id}`);
+    }
+
+    getByVR(id) {
+        return http.get(`/${route}/getByVR/${id}`);
+    }
+
+    getByFUN(id) {
+        return http.get(`/${route}/getByFUN/${id}`);
+    }
+
+    getByPQRS(id) {
+        return http.get(`/${route}/getByPQRS/${id}`);
+    }
+
+    getAllCubXVr() {
+        return http.get(`/${route}/getAllCubXvr`);
+    }
+
+    getByProcess(process) {
+        return http.get(`/${route}/getByProcess/${process}`);
+    }
+
+    createCubXVr(data) {
+        return http.post(`/${route}/createCubXVr`, data);
+    }
+
+    updateCubVr(id, data) {
+        return http.put(`/${route}/updateCubVr/${id}`, data);
+    }
+}
+
+export default new CubXVrDataService();
